56c9d9402d097330b8f4068fcb95be0b9ec9fbdd
Matheus Villela Moreira <matheusvillela@gmail.com>
2018-11-11 22:07:56 -0200

    wont use the okhttp cache

--- app/src/main/java/com/matheusvillela/tmdbupcomingmovies/di/provider/OkHttpProvider.kt
+++ app/src/main/java/com/matheusvillela/tmdbupcomingmovies/di/provider/OkHttpProvider.kt
@@ -1,6 +1,5 @@
 package com.matheusvillela.tmdbupcomingmovies.di.provider
 
-import android.app.Application
 import com.matheusvillela.tmdbupcomingmovies.BuildConfig
 import com.matheusvillela.tmdbupcomingmovies.util.SleepInterceptor
 import com.matheusvillela.tmdbupcomingmovies.util.TmdbKeyInterceptor
@@ -13,15 +12,13 @@ import javax.inject.Provider
 class OkHttpProvider @Inject constructor(
     private val loggingInterceptor: HttpLoggingInterceptor,
     private val keyInterceptor: TmdbKeyInterceptor,
-    private val sleepInterceptor: SleepInterceptor,
-    private val application: Application
+    private val sleepInterceptor: SleepInterceptor
 ) : Provider<OkHttpClient> {
 
     override fun get(): OkHttpClient {
         val builder = OkHttpClient.Builder()
                 .addInterceptor(keyInterceptor)
                 .addInterceptor(loggingInterceptor)
-                //.cache(Cache(File(application.cacheDir, "http"), 1 * 1024 * 1024))
         if (BuildConfig.DEBUG) {
             //builder.addInterceptor(sleepInterceptor)
         }
